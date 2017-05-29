ceddde124d7f1214c4bbf9ac94c46263c4741769
Imran <m.imran8811@gmail.com>
2017-05-29 15:35:57 +0500

    Resourses Addes

--- application/views/index.php
+++ application/views/index.php
@@ -101,40 +101,56 @@
                         <span class="text">Download and print Quote for motivate peoples to help improve community fridge idea</span>
                         <ul>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img1.jpg" alt="quote image">
-                                <a href="images/img1.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-1.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-1.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img2.jpg" alt="quote image">
-                                <a href="images/img2.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-2.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-2.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img9.jpg" alt="quote image">
-                                <a href="images/img9.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-3.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-3.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img1.jpg" alt="quote image">
-                                <a href="images/img1.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-4.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-4.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img2.jpg" alt="quote image">
-                                <a href="images/img2.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-5.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-5.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img9.jpg" alt="quote image">
-                                <a href="images/img9.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-6.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-6.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img1.jpg" alt="quote image">
-                                <a href="images/img1.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-7.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-7.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img2.jpg" alt="quote image">
-                                <a href="images/img2.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-8.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-8.jpg" class="link-download" download>Download</a>
                             </li>
                             <li>
-                                <img src="<?php echo base_url(); ?>assets/images/img9.jpg" alt="quote image">
-                                <a href="images/img9.jpg" class="link-download" download>Download</a>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-9.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-9.jpg" class="link-download" download>Download</a>
+                            </li>
+                            <li>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-10.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-10.jpg" class="link-download" download>Download</a>
+                            </li>
+                            <li>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-11.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-11.jpg" class="link-download" download>Download</a>
+                            </li>
+                            <li>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-12.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-12.jpg" class="link-download" download>Download</a>
+                            </li>
+                            <li>
+                                <img src="<?php echo base_url(); ?>assets/images/thumb-img-13.jpg" alt="quote image">
+                                <a href="<?php echo base_url(); ?>assets/images/poster-img-13.jpg" class="link-download" download>Download</a>
                             </li>
                         </ul>
                     </div>
