d4dff961a26102315d76337512495dee58ad4fa7
Imran <m.imran8811@gmail.com>
2017-06-08 11:07:04 +0500

    final uploads

--- application/views/host_fridge.php
+++ application/views/host_fridge.php
@@ -9,42 +9,42 @@
             <div class="boxes">
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 1 –</h3>
+                        <h3>Step 1</h3>
                         <p> The first thing to do is to check if you have any regulations / restrictions from the government. If so you need to find out the process and then get officially registered / recognized. We are ready to adjust our mapping app to support as needed. Once this is sorted, please communicate with us and we will help get you setup.</p>
                     </div>
                     <div class="clear"></div>
                 </div>
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 2 –</h3>
+                        <h3>Step 2</h3>
                         <p>Get the necessary  permission from the building / housing community, if it is needed.</p>
                     </div>
                     <div class="clear"></div>
                 </div>
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 3 –</h3>
+                        <h3>Step 3</h3>
                         <p> Get a fridge. If your local community has a group on Facebook they might be able to source one for you. Alternatively you can buy your own. When getting a fridge, please check with the manufacture, that it can work outdoors. Additionally find a shaded spot or have a shade built for it.</p>
                     </div>
                     <div class="clear"></div>
                 </div>
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 4 –</h3>
+                        <h3>Step 4</h3>
                         <p>As the says goes, “It takes a village.” The fridge is your baby, but you will need the community to help keep it stocked. A fridge that stays empty helps no one. If there already is a fridge on your street / area. Consider supporting that fridge, rather than setting up your own. Once you have a support system setup, its time to plug it in and get going</p>
                     </div>
                     <div class="clear"></div>
                 </div>
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 5 –</h3>
+                        <h3>Step 5</h3>
                         <p> Stocking the fridge. Be careful about what you put in the fridge. Make sure you fill with approved items and do not violate the local safety regulations. (No raw meats, no partially eaten food, proper containers, nothing past its expiration date, etc.)</p>
                     </div>
                     <div class="clear"></div>
                 </div>
                 <div class="box">
                     <div class="text-area">
-                        <h3>Step 6 –</h3>
+                        <h3>Step 6</h3>
                         <p>Keep your fridge healthy. Keep a schedule to clean it, service it on time.</p>
                     </div>
                     <div class="clear"></div>
