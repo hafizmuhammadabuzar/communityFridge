63a8213ebe29c56d1614212fc9cebbd771bf1713
Imran <m.imran8811@gmail.com>
2017-06-09 11:47:27 +0500

    Admin Side Changes

--- application/views/manager/dashboard.php
+++ application/views/manager/dashboard.php
@@ -7,6 +7,16 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">Dashboard</h1>
+                
+                <div class="row">
+                	<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 mobilefull">
+                    	<div class="BoderBox">
+                            <h4>Fridge</h4>
+                            <img src="<?php echo base_url(); ?>assets/dashboard/dist/images/fridge.png" class="img-responsive" alt="Fridge" />
+						<p>001</p>
+						</div>
+                    </div>
+                </div>
             </div>
             <!-- /.col-lg-12 -->
         </div>
